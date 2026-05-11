9c0edb45fe38ffc9eaab7dbae71308888d55bd11
JuanKy568 <juandiegodiaz996@gmail.com>
2026-05-11 10:12:06 -0500

    Interfaz Django login2

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication', # <--- AÑADE ESTO
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
