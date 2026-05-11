1997f9dc5584736daa2139e83ccc1aeaa1934302
JuanKy568 <juandiegodiaz996@gmail.com>
2026-05-11 10:10:16 -0500

    Interfaz Django login

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication', # <--- AÑADE ESTO
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('api-auth/', include('rest_framework.urls')),
     path('api/users/', include('users.urls')),
     path('api/workouts/', include('workouts.urls')),
     path('api/nutrition/', include('nutrition.urls')),
